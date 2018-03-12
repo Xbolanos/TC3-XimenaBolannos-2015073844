333b291ca7878cb6d9891fb8efd05abdf33066e6
Xbolanos <xbolanosfonseca@gmail.com>
2018-03-11 20:28:50 -0600

    Inicio del Proyecto

---
+++ TC3-XimenaBolannos-2015073844/app/src/main/java/com/example/ximena/tc3_ximenabolannos_2015073844/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ximena.tc3_ximenabolannos_2015073844;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
